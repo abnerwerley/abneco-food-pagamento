4a002c171413ed721c055c1b289f0c82e4295e44
abnerwerley <abner.silva36@aluno.unip.br>
2023-12-13 20:54:25 -0300

    :tada: tada: Initial commit

---
+++ src/test/java/br/com/abneco/food/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abneco.food.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
